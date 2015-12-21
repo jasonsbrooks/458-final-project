8b4a385b946d2a612646cf367000384b406159b7
Jason Brooks <jason.brooks@yale.edu>
2015-12-21 14:30:02 -0500

    Loading screen and basic survey design

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-	$("#signin-facebook-button").click(function() {
-	    $('#resetLayoutForm').submit();
+	setTimeout(function(){
+        $('#preloader').fadeOut('slow',function(){$(this).remove();});
+    }, 1000);
+});
+
+$(window).ready(function() {
+	var left, width, cont = "#q-cont";
+	$('span').click(function() {
+		$(this).css("background", "#5a6");
+		$('.re').css("background", "#d90");
+		next();
 	});
+	$('.re').click(function() {
+		$(".op").css("background", "#d90");
+		$('.re').css("background", "#5a6");
+		$(cont).css("left", "0px");
+	});
+
+	function next() {
+		width = $(cont).width() / 4;
+		left = $(cont).css("left").slice(0, -2) * 1;
+		left -= width;
+		$(cont).css("left", left + "px");
+	}
 });
