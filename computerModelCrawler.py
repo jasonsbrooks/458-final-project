ed4c90d339fd988206646bb177321df8cb2ec26e
Jason Brooks <jason.brooks@yale.edu>
2015-12-21 20:28:36 -0500

    Homepage questions written with new formatting

---
+++ computerModelCrawler.py
@@ -0,0 +1,6 @@
+###################################
+# computerModelCrawler.py         #
+# Grabs and stores information    #
+# about different computer models #
+# parsed from LappyList           #
+###################################
